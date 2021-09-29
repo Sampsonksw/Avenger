f9249c1de7acf995ba0954657a2d9315589169b7
ske <547104456@qq.com>
2021-09-29 22:59:35 +0800

    update decoder.sv id_ex_reg.sv

--- core/milano/rtl/id_ex_reg.sv
+++ core/milano/rtl/id_ex_reg.sv
@@ -10,15 +10,35 @@
 module id_ex_reg(
     input  logic clk_i,
     input  logic rst_ni,
-    input  logic [31:0] instr_i,
-    output logic [31:0] instr_o
+    //input from id
+    ////rs1,rs2 rdata
+    input  logic [31:0] rs1_data_i  ,
+    input  logic [31:0] rs2_data_i  ,
+    ////rd
+    input  logic [4:0]  rd_addr_i   ,       //destination reg addr
+    input  logic        rd_we_i     ,
+
+    //output to ex 
+    ////rs1,rs2 rdata
+    output logic [31:0] rs1_data_ex_o,
+    output logic [31:0] rs2_data_ex_o,
+    ////rd
+    output logic [4:0]  rd_addr_ex_o ,       //destination reg addr
+    output logic        rd_we_ex_o   
+
 );
 
     always_ff @(posedge clk_i,negedge rst_ni)begin
         if (!rst_ni)begin
-            instr_o<= 32'h0;
+            rs1_data_ex_o <=    'h0;
+            rs2_data_ex_o <=    'h0;
+            rd_addr_ex_o  <=    'h0;
+            rd_we_ex_o    <=    'h0;
         end else begin
-            instr_o<=instr_i;
+            rs1_data_ex_o <=    rs1_data_i  
+            rs2_data_ex_o <=    rs2_data_i  
+            rd_addr_ex_o  <=    rd_addr_i   
+            rd_we_ex_o    <=    rd_we_i     
         end
     end
 
